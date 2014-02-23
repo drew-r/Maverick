d51ee0487fcba1184100e6eabac4ffefff7b7f61
Drew Rathbone <drew@rathers.plus.com>
2014-02-23 23:57:32 +0000

    Revised code sample AsyncPatterns

--- codesamples/AsyncPatterns.lua
+++ codesamples/AsyncPatterns.lua
@@ -1,13 +1,20 @@
 #reference 'MavHttp'
+#reference 'MavEntity'
+#reference 'System.Data.Linq.dll'
+#reference 'Newtonsoft.Json'
+#compile '\\demoapi\\entities\\Person.cs'
+
 
 LOG_CLR_CACHE()
-
 serv = Http()
 
 -- asynchronously call a blocking .NET method then call back via scheduler
-async(Thread).Sleep(1000):success(function() print "waited a sec" end)
+local asyncThread = async(Thread) --obtain an async proxy to type Thread 
+asyncThread.Sleep(1000):success(function() print "one" end):error(function(e) print(tostring(e))end)--invoke static method Sleep, on success callback to Lua
+asyncThread.Sleep(1000):success(function() print "two" end)
 
 
+--plain sync
 serv:GET('/',function(req,res)
 
 sw = StreamWriter(res.OutputStream)
@@ -24,7 +31,7 @@ sw = StreamWriter(res.OutputStream)
 sw.AutoFlush = true
 
 -- _async mirrors the global table but wraps its elements with async(), providing an alternative syntax. 
-_async.sw:Write("Hello Internet! (async)",1):success(function() res:Close() end)
+_async.sw:Write("Hello Internet! (async)",1):success(function() res:Close() end):error(function(e) print(tostring(e)) end)
 	
 end)
 
@@ -41,12 +48,26 @@ res.OutputStream:BeginWrite(resBuffer,0,resBuffer.Length,
 end)
 
 
-serv:GET('/close',function(req,res)
+serv:GET('/data',function(req,res)
 
---a quick method of closing a response asynchronously
-res:Close(Encoding.UTF8:GetBytes("Hello Internet! (close)"),false)
+local data = EntityContext("Server=localhost;Database=demoapi;Trusted_Connection=True;") 
+_async.JSON.stringify(Q(data.Person)):success(function(x)
+			sw = StreamWriter(res.OutputStream)
+			_async.sw:Write(x):success(function() res:Close() end)
+										end):error(function(e) res:Close(Encoding.UTF8:GetBytes(tostring(e))) end)
 
 end)
+
+serv:GET('/data_sync',function(req,res)
+
+local data = EntityContext("Server=localhost;Database=demoapi;Trusted_Connection=True;") 
+
+sw = StreamWriter(res.OutputStream)
+sw:Write(JSON.stringify(Q(data.Person)))
+res:Close()
+
+end)
+
 
 serv:listen("http://+:8080/")
 
