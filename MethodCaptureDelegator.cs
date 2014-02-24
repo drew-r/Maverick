e8a5eb7b0f50c2894d40c5afe4a05ab9ae48abff
Drew Rathbone <drew@rathers.plus.com>
2014-02-24 10:22:48 +0000

    MethodCaptureDelegator: revised method resolution

--- MethodCaptureDelegator.cs
+++ MethodCaptureDelegator.cs
@@ -23,12 +23,11 @@ namespace Maverick
         public object this[string idx]
         {
             get
-            {
-                if (_methodName != null) { throw new InvalidOperationException("Method already set."); }
+            {                   
                 _methodName = idx;
                 return this;
             }
-        }       
+        }
 
         public object go(object[] args)
         {
@@ -37,34 +36,71 @@ namespace Maverick
             { 
                 args = args.Take(args.Length-1).ToArray();
             }
-            
+
             return new Func<object>(() => {
                                
                 if (!_static) { args = args.Skip(1).ToArray(); }
+                 MethodBase method = resolveMethod(args);
 
-                MethodBase method = null;
-                int i = 0;
-                    do
-                    {
-
-                        if (i > args.Length) { throw new Exception("Could not resolve method " + _methodName + " from type " + _targetType.ToString() + " target " + _target.ToString()); }
-                        int iters = 0;
-                        method = _targetType.GetMethod(_methodName, args.Select((x) =>
-                        {
-                            Type type = x.GetType();
-                            if (type == typeof(double) && iters <= i) { args[iters] = Convert.ToInt32(args[iters]); type = typeof(int); }
-                            iters++;
-                            return type;
-                        }).ToArray());
-                        i++;
-                    }
-                    while (method == null);
-               
                 return method.Invoke(_target, args); 
             
-            });
-        
+            });         
         }
+
+  
+
+        MethodBase resolveMethod(object[] args)
+        {
+               
+            MethodInfo method = null;                            
+          
+            Type[] argTypes = args.Select(a => a.GetType()).ToArray();
+            
+            IEnumerable<MethodInfo> methodNameMatches = _targetType.GetMethods().Where((mi) => mi.Name == _methodName);
+            foreach (MethodInfo methodNameMatch in methodNameMatches)
+            {
+                ParameterInfo[] pi = methodNameMatch.GetParameters();
+                if (pi.Length != argTypes.Length) { continue; }
+                bool match = true;
+                for (int p_idx = 0; p_idx < pi.Length; p_idx++)
+                {
+                    Type parameterType = pi[p_idx].ParameterType;
+                    Type argType = argTypes[p_idx];
+                    object arg = args[p_idx];
+                    
+                    if (!parameterType.IsAssignableFrom(argType))
+                    {
+                        if (argType == typeof(double))
+                        {
+                            if (parameterType == typeof(Int16))
+                            {
+                                args[p_idx] = Convert.ToInt16(arg);
+                                continue;
+                            }
+                            if (parameterType == typeof(Int32))
+                            {
+                                args[p_idx] = Convert.ToInt32(arg);
+                                continue;
+                            }
+                        }                       
+                        match = false;
+                    }
+                }
+
+                if (match)
+                {
+                    method = methodNameMatch;
+                }
+            }
+            if (method == null)
+                throw new Exception("Could not resolve method " + _methodName + " from type " + _targetType.ToString() + " target " + _target.ToString() +
+                    " with argument types " + String.Join(", ", argTypes.Select(at => at.Name)) + ".");
+           
+
+            return method;
+        }
+
+
         public object go()
         {
             return go(null);
