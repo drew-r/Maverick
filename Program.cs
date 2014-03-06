5c7a8baa364e8ce768953170bc385efd54be8197
Drew Rathbone <drew@rathers.plus.com>
2014-03-06 21:31:43 +0000

    Program.cs: Console hiding functionality removed
    In interests of linux compatibility

--- Program.cs
+++ Program.cs
@@ -15,19 +15,19 @@ namespace Maverick
 {
     public static class Program
     {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
+        //[DllImport("kernel32.dll")]
+        //static extern IntPtr GetConsoleWindow();
+        //[DllImport("user32.dll")]
+        //static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        //const int SW_HIDE = 0;
+        //const int SW_SHOW = 5;
         public static void HideConsole()
         {
-            ShowWindow(GetConsoleWindow(), SW_HIDE);
+            //ShowWindow(GetConsoleWindow(), SW_HIDE);
         }
         public static void ShowConsole()
         {
-            ShowWindow(GetConsoleWindow(), SW_SHOW);
+            //ShowWindow(GetConsoleWindow(), SW_SHOW);
         }
         static void Main(params string[] args) 
         {
